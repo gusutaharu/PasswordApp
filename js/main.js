2df44b552bc85285e1796fbbf94f3c0ca8cadba9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-15 09:40:23 +0900

    lesson10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 'use strict';
 {
-  
+  const slider = document.getElementById('slider');
+
+  slider.addEventListener('input', ()=>{
+    const passwordLength = document.getElementById('password-length');
+
+    passwordLength.textContent = slider.value
+  });
 }
