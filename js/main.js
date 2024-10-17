4d58e2d12ff29967bb57f6f99db3cd2c4badf5cd
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-17 22:40:37 +0900

    lesson16

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 'use strict';
 {
-  const slider = document.getElementById('slider');
-  const btn = document.getElementById('btn');
-
-  slider.addEventListener('input', ()=>{
-    const passwordLength = document.getElementById('password-length');
-
-    passwordLength.textContent = slider.value
-  });
-  btn.addEventListener('click', ()=>{
+  function showPassword() {
     const result = document.getElementById('result');
     const numbersCheckbox = document.getElementById('numbers-checkbox');
     const symbolsCheckbox = document.getElementById('symboles-checkbox');
@@ -30,5 +22,19 @@
       password += seed[Math.floor(Math.random()*seed.length)];
     }
     result.textContent = password;
+  };
+
+  const slider = document.getElementById('slider');
+  const btn = document.getElementById('btn');
+
+  slider.addEventListener('input', ()=>{
+    const passwordLength = document.getElementById('password-length');
+
+    passwordLength.textContent = slider.value
   });
+  btn.addEventListener('click', ()=>{
+    showPassword();
+  });
+
+  showPassword();
 }
